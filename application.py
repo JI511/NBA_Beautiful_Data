28670c6f098b934b32f7222fb14438e9ace12089
John Ingwersen <johningwersen11@gmail.com>
2019-10-07 20:41:39 -0500

    Get most recent box score for a player

--- application.py
+++ application.py
@@ -3,6 +3,7 @@
 # ----------------------------------------------------------------------------------------------------------------------
 
 # imports
+import datetime
 from basketball_reference_web_scraper import client
 from basketball_reference_web_scraper.data import Team
 
@@ -15,19 +16,32 @@ class Application(object):
         """
         Setup for the application.
         """
-        pass
+        self.player = 'LeBron James'
 
     def run(self):
         """
         Runs the application.
         """
-        self.get_box_score()
+        print("The Latest box score for Lebron James")
+        date = datetime.datetime.today()
+        date = datetime.datetime(year=2019, month=4, day=4)
+        while True:
+            print('Attempting date: %s' % date.strftime('%y-%m-%d'))
+            if self.get_box_score(day=date.day, month=date.month, year=date.year):
+                break
+            date -= datetime.timedelta(days=1)
 
-    def get_box_score(self):
-        box_scores = client.player_box_scores(day=13, month=6, year=2019)
-        print('***** BOX SCORES *****')
-        for box_score in box_scores[:10]:
-            print(box_score)
+    def get_box_score(self, day, month, year):
+        found = False
+        box_scores = client.player_box_scores(day=day, month=month, year=year)
+        print('Total players on this date: %s' % len(box_scores))
+        for box_score in box_scores:
+            # print(box_score)
+            if self.player in box_score['name']:
+                print(box_score)
+                found = True
+                break
+        return found
 
 # ----------------------------------------------------------------------------------------------------------------------
 # End
