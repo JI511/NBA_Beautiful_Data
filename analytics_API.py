2181661c20569ff670f91eb8bb71a229d807737d
John Ingwersen <johningwersen11@gmail.com>
2019-10-12 15:14:50 -0500

    API class

---
+++ analytics_API.py
@@ -0,0 +1,43 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Analytics API
+# ----------------------------------------------------------------------------------------------------------------------
+
+# imports
+import datetime
+from basketball_reference_web_scraper import client
+from basketball_reference_web_scraper.data import Team
+
+
+def get_play_box_score(name, date_obj=None, timeout=3):
+    """
+    Gets the box score for the desired player.
+
+    :param name: Name of the player to search for.
+    :param datetime.datetime date_obj: Datetime object for starting day to search.
+    :param int timeout: Number of days to search before giving up.
+    :return: Box score for the player if found.
+    :rtype: dict
+    """
+    date = date_obj
+    if date_obj is None:
+        date = datetime.datetime.today()
+    bs = None
+    while True:
+        print('Attempting date: %s' % date.strftime('%y-%m-%d'))
+        found = False
+        box_scores = client.player_box_scores(day=date.day, month=date.month, year=date.year)
+        print('Total players on this date: %s' % len(box_scores))
+        for box_score in box_scores:
+            # print(box_score)
+            if name in box_score['name'].lower():
+                bs = box_score
+                found = True
+                break
+        if found:
+            break
+        date -= datetime.timedelta(days=1)
+    return bs
+
+# ----------------------------------------------------------------------------------------------------------------------
+# End
+# ----------------------------------------------------------------------------------------------------------------------
--- application.py
+++ application.py
@@ -6,6 +6,7 @@
 import datetime
 from basketball_reference_web_scraper import client
 from basketball_reference_web_scraper.data import Team
+import analytics_API as api
 
 
 class Application(object):
@@ -16,32 +17,17 @@ class Application(object):
         """
         Setup for the application.
         """
-        self.player = 'LeBron James'
+        self.player = 'LeBron James'.lower()
 
     def run(self):
         """
         Runs the application.
         """
         print("The Latest box score for Lebron James")
-        date = datetime.datetime.today()
-        date = datetime.datetime(year=2019, month=4, day=4)
-        while True:
-            print('Attempting date: %s' % date.strftime('%y-%m-%d'))
-            if self.get_box_score(day=date.day, month=date.month, year=date.year):
-                break
-            date -= datetime.timedelta(days=1)
+        # date = datetime.datetime.today()
+        date = datetime.datetime(year=2019, month=3, day=29)
+        print(api.get_play_box_score(name=self.player, date_obj=date))
 
-    def get_box_score(self, day, month, year):
-        found = False
-        box_scores = client.player_box_scores(day=day, month=month, year=year)
-        print('Total players on this date: %s' % len(box_scores))
-        for box_score in box_scores:
-            # print(box_score)
-            if self.player in box_score['name']:
-                print(box_score)
-                found = True
-                break
-        return found
 
 # ----------------------------------------------------------------------------------------------------------------------
 # End
