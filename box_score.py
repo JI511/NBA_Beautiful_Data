fb1e5ca800ce9e8820fb3cfe31354ea4d5243651
John Ingwersen <johningwersen11@gmail.com>
2019-10-16 22:11:04 -0500

    Added calc points

--- box_score.py
+++ box_score.py
@@ -17,7 +17,12 @@ class BoxScore(object):
         """
         self.keys = ['name', 'points', 'rebounds', 'assists']
         self.player_name = bs_dict['name']
-        self.points = bs_dict['points']
+        self.made_fg = bs_dict['made_field_goals']
+        self.made_threes = bs_dict['made_three_point_field_goals']
+        self.made_ft = bs_dict['made_free_throws']
+        self.points = self.__calculate_points(made_fg=self.made_fg,
+                                              made_three=self.made_threes,
+                                              made_ft=self.made_ft)
         self.rebounds = bs_dict['rebounds']
         self.assists = bs_dict['assists']
 
@@ -25,23 +30,32 @@ class BoxScore(object):
         """
         Converts the box score object into an easily readable string.
         :return: Box score data
+        :rtype: str
         """
-        print('Name: %s' % self.player_name)
-        print('Points: %s' % self.points)
-        print('Rebounds: %s' % self.rebounds)
-        print('Assists: %s' % self.assists)
+        box_score = ''
+        box_score += 'Name: %s' % self.player_name
+        box_score += 'Points: %s' % self.points
+        box_score += 'Rebounds: %s' % self.rebounds
+        box_score += 'Assists: %s' % self.assists
+        return box_score
 
-    def __calculate_points(self, made_fg, made_three, made_ft):
+    @staticmethod
+    def __calculate_points(made_fg, made_three, made_ft):
         """
         Calculates the number of points scored from the combinations of all made baskets.
 
-        :param made_fg:
-        :param made_three:
-        :param made_ft:
+        :param int made_fg: Number of made field goals.
+        :param int made_three: Number of made three point field goals.
+        :param int made_ft: Number of made free throws.
         :return: The number of points scored.
         :rtype: int
         """
-        pass
+        points = 0
+        fg = made_fg - made_three
+        points += (made_three * 3)
+        points += (fg * 2)
+        points += made_ft
+        return points
 
 # ----------------------------------------------------------------------------------------------------------------------
 # End
