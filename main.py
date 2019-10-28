ca9a9f3f8e0858a7821583c7e0875963fa63f181
John Ingwersen <johningwersen11@gmail.com>
2019-10-27 19:29:01 -0500

    Moved some sloc

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ import application
 import datetime
 import argparse
 
-date = datetime.datetime.now()
 parser = argparse.ArgumentParser()
 parser.add_argument('--yesterday', help='The date to search will be the previous day.',
                     dest='yesterday', action='store_true')
 parser.set_defaults(yesterday=True)
 args = parser.parse_args()
 
+date = datetime.datetime.now()
 if args.yesterday:
     date -= datetime.timedelta(days=1)
 
