db7b8701dccd4105928ad8348e08c1f4aecbe618
John Ingwersen <johningwersen11@gmail.com>
2019-11-02 10:30:34 -0500

    Added main command line args

--- main.py
+++ main.py
@@ -12,7 +12,11 @@ parser.add_argument('--yesterday', help='The date to search will be the previous
                     dest='yesterday', action='store_true')
 parser.add_argument('--log', help='Indicates if logging/log file should be used.',
                     dest='log', action='store_true')
-parser.set_defaults(yesterday=False, log=False)
+parser.add_argument('--plot', help='Indicates if plots should be created.',
+                    dest='plot', action='store_true')
+parser.add_argument('--gather_new', help='Indicates if new player box score data should be searched for.',
+                    dest='gather_new', action='store_true')
+parser.set_defaults(yesterday=False, log=False, plot=False, gather_new=False)
 args = parser.parse_args()
 
 date = datetime.datetime.now()
@@ -22,7 +26,7 @@ else:
     date = False
 
 app = application.Application()
-app.run(date=date, should_log=args.log)
+app.run(date=date, should_log=args.log, plot=args.plot, gather_new=args.gather_new)
 
 
 # ----------------------------------------------------------------------------------------------------------------------
