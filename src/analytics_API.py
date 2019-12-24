18f39ef91b4364054d5e5742d2095baf6c5970cf
John Ingwersen <johningwersen11@gmail.com>
2019-12-24 15:50:07 -0600

    Fixed bug returning the wrong DataFrame object.
    The old returned temp_df was filtered on the x and y keys, and did not contain any other columns.

--- src/analytics_API.py
+++ src/analytics_API.py
@@ -463,7 +463,7 @@ def create_scatter_plot_with_trend_line(x_key, y_key, df, **kwargs):
             plot_path = save_path
     if show_plot:
         plt.show()
-    return plot_path, outlier_df, temp_df
+    return plot_path, outlier_df, df
 
 
 def create_bar_plot(df, bar_items, save_path=None, show_plot=False, team=None, date=None):
